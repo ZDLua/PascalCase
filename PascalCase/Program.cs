ca4d2bda865866d95255bcd879f1a209505f50dd
ZDLua <hj_jr@icloud.com>
2022-08-02 09:30:22 -0500

    first

---
+++ PascalCase/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PascalCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string catName = "Terry";
+            int catAge = 12;
+            int dayNum = 2;
+            int monthDay = 2;
+            int year = 2020;
+
+            Console.WriteLine($"My cat's name is {catName}, He is {catAge} years old. He was adopted on {monthDay}/{dayNum}/{year} ");
+        }
+    }
+}
+
